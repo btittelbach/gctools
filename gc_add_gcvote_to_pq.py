1f2e8d15c967babbf9d126f3a434f05c13762e2e
Bernhard Tittelbach <bernhard@tittelbach.org>
2012-03-18 04:22:22 +0100

    fix html for garmin devices, add --mean switch

--- gc_add_gcvote_to_pq.py
+++ gc_add_gcvote_to_pq.py
@@ -7,18 +7,20 @@ from lxml import etree
 import requests
 #import re
 
-show_vote_string_="GCVote: %s (%s votes)\n"
+show_vote_string_="GCVote: %s (%s votes)"
 gcvote_getvote_uri_='http://gcvote.com/getVotes.php'
 gc_guid_uri_='http://www.geocaching.com/seek/cache_details.aspx?guid='
 xml_parser_ = etree.XMLParser(encoding="utf-8")
+use_median_=True
 
 def usage():
   print "Sytax:"
   print "       %s [options] <pocketquery.gpx> [...]" % (sys.argv[0])
   print "Options:"
-  print "       -h           | --help"
-  print "       -u username  | --username=gcvote_user"
-  print "       -p password  | --password=gcvote_pass"
+  print "       -h          | --help"
+  print "       -u username | --username=gcvote_user"
+  print "       -p password | --password=gcvote_pass"
+  print "       -m          | --mean     Use mean instead of median"
 
 def splitList(lst,n):
   i=0
@@ -51,11 +53,16 @@ if __name__ == '__main__':
   gcvote_username_ = None
   gcvote_password_ = None
   try:
-    opts, files = getopt.gnu_getopt(sys.argv[1:], "hu:p:", ["user=","pass=","help"])
+    opts, files = getopt.gnu_getopt(sys.argv[1:], "mhu:p:", ["user=","pass=","help","mean"])
   except getopt.GetoptError as e:
     print "ERROR: Invalid Option: " +str(e)
     usage()
     sys.exit(1)
+
+  if len(files) < 1:
+    print "ERROR: no gpx file given\n"
+    usage()
+    sys.exit()
 
   for o, a in opts:
     if o in ["-h","--help"]:
@@ -65,6 +72,8 @@ if __name__ == '__main__':
       gcvote_username_ = a
     elif o in ["-p","--pass"]:
       gcvote_password_ = a
+    elif o in ["-m","--mean"]:
+      use_median_=False
 
   for gpxfile in files:
     tree = None
@@ -79,19 +88,14 @@ if __name__ == '__main__':
       continue
     urls = [ url.text for url in tree.iterfind(".//{http://www.topografix.com/GPX/1/0}url",namespaces=tree.nsmap) ]
     gcids += map(lambda x: x[len(gc_guid_uri_):], filter(lambda x: x.startswith(gc_guid_uri_), urls))
-    votes_dict = getGCVotes(gcvote_username_, gcvote_password_, gcids)
+    votes_dict = getGCVotes(gcvote_username_, gcvote_password_, gcids, use_median=use_median_)
     for wpt_elem in tree.iterfind("{http://www.topografix.com/GPX/1/0}wpt",namespaces=tree.nsmap):
       gccode = wpt_elem.find("{http://www.topografix.com/GPX/1/0}name",namespaces=tree.nsmap).text
       sdesc_elem = wpt_elem.find(".//{http://www.groundspeak.com/cache/1/0}short_description",namespaces=tree.nsmap)
       if sdesc_elem is not None and gccode in votes_dict:
         vote_info = show_vote_string_ % votes_dict[gccode]
-        if sdesc_elem.get("html") == "False":
-          sdesc_elem.text = vote_info+sdesc_elem.text
-        else:
-          votepelem = etree.Element("p",nsmap=tree.nsmap)
-          votepelem.text=vote_info
-          votepelem.append(etree.Element("br",nsmap=tree.nsmap))
-          sdesc_elem.insert(0,votepelem)
+        sdesc_elem.set("html","True")
+        sdesc_elem.text = vote_info+"<br/>\n"+sdesc_elem.text
     with open(gpxfile, mode="wb") as f:
       f.truncate()
       f.write(etree.tostring(tree,method="xml",xml_declaration=True,encoding="utf-8"))
