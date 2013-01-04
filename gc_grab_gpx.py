0d95ab952db17baec4a63676c25bd497113147ac
Bernhard Tittelbach <bernhard@tittelbach.org>
2013-01-04 04:36:10 +0100

    use lib and save auth cookie

--- gc_grab_gpx.py
+++ gc_grab_gpx.py
@@ -10,15 +10,11 @@ import getopt
 from lxml import etree
 import requests
 import re
+import geocachingsitelib as gc
 
 destination_dir_=os.path.curdir
 gc_username_ = None
 gc_password_ = None
-gc_wp_uri_="http://www.geocaching.com/seek/cache_details.aspx?wp=%s"
-gc_auth_uri_="https://www.geocaching.com/login/default.aspx"
-gc_pqlist_uri_="http://www.geocaching.com/pocket/default.aspx"
-gc_pqdownload_host_="http://www.geocaching.com"
-gc_pqdownload_path_='/pocket/downloadpq.ashx?g=%s'
 
 def usage():
     print "Sytax:"
@@ -31,9 +27,12 @@ def usage():
     print "       -c           | --createpqdir      Create dir for PQ"
     print "       -u username  | --username=gc_user "
     print "       -p password  | --password=gc_pass "
+    print "       -i           | --noninteractive   Never prompt for pwd, just fail"
+    print "If username and password are not provided, we interactively"
+    print "ask for them the first time and store a session cookie. Unless -i is given"
 
 try:
-    opts, args = getopt.gnu_getopt(sys.argv[1:], "u:p:hlad:c", ["listpq","help","gpxdir=","username=","password=","allpq","createpqdir"])
+    opts, args = getopt.gnu_getopt(sys.argv[1:], "u:p:hlad:ci", ["listpq","help","gpxdir=","username=","password=","allpq","createpqdir","noninteractive"])
 except getopt.GetoptError, e:
     print "ERROR: Invalid Option: " +str(e)
     usage()
@@ -42,6 +41,7 @@ except getopt.GetoptError, e:
 fetch_all_pqs_ = False
 list_pqs_ = False
 create_pq_dir_ = False
+be_interactive_ = True
 for o, a in opts:
     if o in ["-h","--help"]:
         usage()
@@ -58,105 +58,43 @@ for o, a in opts:
         gc_username_ = a
     elif o in ["-p","--password"]:
         gc_password_ = a
+    elif o in ["-i","--noninteractive"]:
+        be_interactive_ = False
 
 re_gccode = re.compile(r'GC[a-z0-9]{1,6}',re.IGNORECASE)
 re_pquid = re.compile(r'[a-f0-9-]{36}',re.IGNORECASE)
 pquids = filter(re_pquid.match, args)
 gccodes = filter(re_gccode.match, args)
 pqnames = set(args) - set(pquids) - set(gccodes)
+destination_dir_ = os.path.expanduser(destination_dir_)
+
 if (list_pqs_ == False and fetch_all_pqs_ == False and len(args) <1) or not os.path.isdir(destination_dir_):
     print "ERROR: No gccodes given or %s may not be a valid writeable directory" % destination_dir_
     usage()
     sys.exit(1)
 
-if gc_username_ is None or gc_password_ is None:
-    print "ERROR: username and/or password not given"
-    usage()
-    sys.exit(1)
-
-destination_dir_=os.path.expanduser(destination_dir_)
-user_agent_="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:10.0.2) Gecko/20100101 Firefox/10.0.2"
-
-if  "__build__" in requests.__dict__ and requests.__build__ >= 0x000704:
-    cookie_jar_ = {}
-else:
-    import cookielib
-    cookie_jar_ =cookielib.CookieJar()
-
-parser_ = etree.HTMLParser(encoding="utf-8")
-
-def gc_login(usr, pwd):
-    global cookie_jar_
-    post_data={"__EVENTTARGET":"","__EVENTARGUMENT":"","ctl00$ContentBody$tbUsername":usr,"ctl00$ContentBody$tbPassword":pwd,"ctl00$ContentBody$btnSignIn":"Login"}
-    r = requests.post(gc_auth_uri_, data=post_data, allow_redirects=False, cookies=cookie_jar_, headers={"User-Agent":user_agent_})
-    if  "__build__" in requests.__dict__ and requests.__build__ >= 0x000704:
-        cookie_jar_ = r.cookies
-        return r.error is None and "userid" in r.cookies
+try:
+    if gc_username_ is None or gc_password_ is None:
+        gc.autologin_interactive_save_cookie(be_interactive_)
     else:
-        return r.error is None and re.sub(r"<[^>]*>","",r.content).find('You are logged in as %s' % (usr)) > -1
-
-def gc_download_gpx(gccode, dstdir):
-    global cookie_jar_
-    uri = gc_wp_uri_ % gccode.upper()
-    r = requests.get(uri, cookies=cookie_jar_, headers={"User-Agent":user_agent_, "Referer":uri})
-    post_data={"ctl00$ContentBody$btnGPXDL":"GPX file"}
-    if r.error is None:
-        tree = etree.fromstring(r.content, parser_)
-        for input_elem in tree.findall(".//input[@type='hidden']"):
-            post_data[input_elem.get("name")] = input_elem.get("value")
-        r = requests.post(uri, data=post_data, allow_redirects=True, cookies=cookie_jar_, headers={"User-Agent":user_agent_, "Referer":uri})
-        if r.error is None:
-            cd_header = "attachment; filename="
-            if "content-disposition" in r.headers and r.headers["content-disposition"].startswith(cd_header):
-                filename=r.headers["content-disposition"][len(cd_header):]
-                with open(os.path.join(dstdir,filename),"wb") as fh:
-                    fh.write(r.content)
-                    return filename
-            raise Exception("Invalid gccode or other geocaching.com error")
-    raise Exception("Recieved HTTP Error "+str(r.status_code))
-
-def gc_get_pq_names():
-    global cookie_jar_
-    uri = gc_pqlist_uri_
-    r = requests.get(uri, cookies=cookie_jar_, headers={"User-Agent":user_agent_, "Referer":uri})
-    if r.error is None:
-        rv = {}
-        tree = etree.fromstring(r.content, parser_)
-        for a_elem in tree.findall(".//a[@href]"):
-            if a_elem.get("href").startswith(gc_pqdownload_path_ % ""):
-                rv[a_elem.text.strip()] = a_elem.get("href")[len(gc_pqdownload_path_ % ""):]
-        return rv
-    raise Exception("Recieved HTTP Error "+str(r.status_code))
-
-def gc_download_pq(pquid, dstdir):
-    global cookie_jar_
-    uri = gc_pqdownload_host_ + gc_pqdownload_path_ % pquid
-    r = requests.get(uri, cookies=cookie_jar_)
-    if r.error is None:
-        cd_header = "attachment; filename="
-        if "content-disposition" in r.headers and r.headers["content-disposition"].startswith(cd_header):
-            filename=r.headers["content-disposition"][len(cd_header):]
-            with open(os.path.join(dstdir,filename),"wb") as fh:
-                fh.write(r.content)
-                return filename
-        raise Exception("Invalid PQ uid or other geocaching.com error")
-    raise Exception("Recieved HTTP Error "+str(r.status_code))
+        gc.login(gc_username_, gc_password_)
+except Exception, e:
+    gc.autologin_invalidate_cookie()
+    print "ERROR during authentication/login"
+    print e
+    sys.exit(1)
 
 pqdict = {}
 pqrevdict = {}
 pq_to_get_tuplelist = []
 
-if not gc_login(gc_username_, gc_password_):
-    print "ERROR: login failed, wrong username/password"
-    sys.exit(1)
-
 if len(pqnames) > 0 or len(pquids) > 0 or fetch_all_pqs_ or list_pqs_:
-    pqdict = gc_get_pq_names()
+    pqdict = gc.get_pq_names()
     pqrevdict = dict(zip(pqdict.values(),pqdict.keys()))
 
 for gccode in gccodes:
     try:
-        fn = gc_download_gpx(gccode, destination_dir_)
+        fn = gc.download_gpx(gccode, destination_dir_)
         print "downloaded %s to %s" % (fn, destination_dir_)
     except Exception, e:
         print "ERROR: GPX download of %s to %s failed" % (gccode, destination_dir_)
@@ -190,7 +128,7 @@ for (pqname,pquid) in pq_to_get_tuplelist:
     try:
         if not os.path.exists(pq_save_dir):
             os.mkdir(pq_save_dir)
-        fn = gc_download_pq(pquid, pq_save_dir)
+        fn = gc.download_pq(pquid, pq_save_dir)
         print "downloaded %s and saved %s to %s" % (pqname, fn, pq_save_dir)
     except Exception, e:
         print "ERROR: download of PQ '%s' with id '%s' to %s failed" % (pqname, pquid, pq_save_dir)
--- geocachingsitelib.py
+++ geocachingsitelib.py
@@ -11,21 +11,38 @@ from lxml import etree
 import requests
 import re
 import cPickle
+import exceptions
+
+#### Global Constants ####
 
 gc_auth_uri_="https://www.geocaching.com/login/default.aspx"
-gc_upload_uri_="http://www.geocaching.com/my/fieldnotes.aspx"
+gc_uploadfieldnotes_uri_="http://www.geocaching.com/my/uploadfieldnotes.aspx"
 gc_wp_uri_="http://www.geocaching.com/seek/cache_details.aspx?wp=%s"
-gc_auth_uri_="https://www.geocaching.com/login/default.aspx"
 gc_pqlist_uri_="http://www.geocaching.com/pocket/default.aspx"
 gc_pqdownload_host_="http://www.geocaching.com"
 gc_pqdownload_path_='/pocket/downloadpq.ashx?g=%s'
 
 user_agent_="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:10.0.2) Gecko/20100101 Firefox/10.0.2"
 
-def is_new_requests_lib():
+default_config_dir_= os.path.join(os.path.expanduser('~'),".local","share","gctools")
+auth_cookie_default_filename_= "gctools_cookies"
+
+
+#### Exceptions ####
+
+class HTTPError(Exception):
+    pass
+
+class GeocachingSiteError(Exception):
+    pass
+
+
+#### Internal Helper Functions ####
+
+def _is_new_requests_lib():
     return "__build__" in requests.__dict__ and requests.__build__ >= 0x000704
 
-if is_new_requests_lib():
+if _is_new_requests_lib():
     cookie_jar_ = {}
 else:
     import cookielib
@@ -33,30 +50,15 @@ else:
 
 parser_ = etree.HTMLParser(encoding="utf-8")
 
-def save_cookie_login(cookie_filepath):
-    global cookie_jar_
-    saved_data = {
-        "jar":cookie_jar_,
-        "requestsversion": requests.__build__ if  "__build__" in requests.__dict__ else None
-    }
-    with open(cookie_filepath, "wb") as fh:
-        cPickle.dump(saved_data,fh,version=2)
+def _ask_usr_pwd():
+    print "Please provide your geocaching.com login credentials:"
+    sys.stdout.write("username: ")
+    usr = sys.stdin.readline().strip()
+    sys.stdout.write("password: ")
+    pwd = sys.stdin.readline().strip("\n")
+    return (usr,pwd)
 
-def load_cookie_login(cookie_filepath):
-    global cookie_jar_
-    with open(cookie_filepath, "rb") as fh:
-        saved_data = cPickle.load(fh,version=2)
-    if not ("jar" in saved_data and "requestsversion" in saved_data):
-        raise Exception("No Cookies in this pickle jar")
-    if is_new_requests_lib():
-        if saved_data["requestsversion"] is None or saved_data["requestsversion"] > requests.__build__:
-            raise Exception("given cookie file is not compatible")
-    else:
-        if not saved_data["requestsversion"] is None:
-            raise Exception("given cookie file is not compatible")
-    cookie_jar_ = saved_data["jar"]
-
-def parse_for_hidden_inputs(uri):
+def _parse_for_hidden_inputs(uri):
     global cookie_jar_
     post_data = {}
     r = requests.get(uri, cookies=cookie_jar_, headers={"User-Agent":user_agent_, "Referer":uri})
@@ -65,8 +67,47 @@ def parse_for_hidden_inputs(uri):
         for input_elem in tree.findall(".//input[@type='hidden']"):
             post_data[input_elem.get("name")] = input_elem.get("value")
     return post_data
-    
-def gc_login(usr, pwd, remember=False):
+
+def _open_config_file(filename, mode):
+    if not os.path.isdir(default_config_dir_):
+        os.makedirs(default_config_dir_)
+    filepath = os.path.join(default_config_dir_, os.path.basename(filename))
+    return open(filepath, mode)
+
+def _delete_config_file(filename):
+    filepath = os.path.join(default_config_dir_, os.path.basename(filename))
+    try:
+        os.unlink(filepath)
+    except:
+        pass
+
+def _save_cookie_login(cookie_fileobject):
+    global cookie_jar_
+    saved_data = {
+        "jar":cookie_jar_,
+        "requestsversion": requests.__build__ if  "__build__" in requests.__dict__ else None
+    }
+    assert(cookie_fileobject.mode == "wb")
+    cPickle.dump(saved_data, cookie_fileobject, 2)
+
+def _load_cookie_login(cookie_fileobject):
+    global cookie_jar_
+    assert(cookie_fileobject.mode == "rb")
+    saved_data = cPickle.load(cookie_fileobject)
+    if not ("jar" in saved_data and "requestsversion" in saved_data):
+        raise Exception("No Cookies in this pickle jar")
+    if _is_new_requests_lib():
+        if saved_data["requestsversion"] is None or saved_data["requestsversion"] > requests.__build__:
+            raise Exception("given cookie file is not compatible")
+    else:
+        if not saved_data["requestsversion"] is None:
+            raise Exception("given cookie file is not compatible")
+    cookie_jar_ = saved_data["jar"]
+
+
+#### Library Functions ####
+
+def login(usr, pwd, remember=False):
     global cookie_jar_
     post_data = {
         "__EVENTTARGET":"",
@@ -78,87 +119,105 @@ def gc_login(usr, pwd, remember=False):
     if remember:
         post_data["ctl00$ContentBody$cbRememberMe"]="1"
     r = requests.post(gc_auth_uri_, data=post_data, allow_redirects=False, cookies=cookie_jar_, headers={"User-Agent":user_agent_})
-    if is_new_requests_lib():
+    login_ok = False
+    if _is_new_requests_lib():
         cookie_jar_ = r.cookies
-        return r.error is None and "userid" in r.cookies
+        login_ok = r.error is None and "userid" in r.cookies
     else:
-        return r.error is None and re.sub(r"<[^>]*>","",r.content).find('You are logged in as %s' % (usr)) > -1
+        login_ok = r.error is None and re.sub(r"<[^>]*>","",r.content).find('You are logged in as %s' % (usr)) > -1
+    if not login_ok:
+        raise GeocachingSiteError("login failed, wrong username/password")
 
-def gc_download_gpx(gccode, dstdir):
+def autologin_invalidate_cookie():
+    _delete_config_file(auth_cookie_default_filename_)    
+
+def autologin_interactive_save_cookie(be_interactive = True):
+    global cookie_jar_
+    try:
+        _load_cookie_login(_open_config_file(auth_cookie_default_filename_,"rb"))
+        #TODO: check if session has timed out or cookie expired
+    except:
+        if be_interactive:
+            (usr,pwd) = _ask_usr_pwd()
+            login(usr, pwd, True)
+            _save_cookie_login(_open_config_file(auth_cookie_default_filename_,"wb"))
+        else:
+            autologin_invalidate_cookie()
+            raise Exception("Missing or invalid login-cookie-file")
+
+def download_gpx(gccode, dstdir):
     global cookie_jar_
     uri = gc_wp_uri_ % gccode.upper()
     post_data={"ctl00$ContentBody$btnGPXDL":"GPX file"}
-    post_data.update(parse_for_hidden_inputs(uri))
+    post_data.update(_parse_for_hidden_inputs(uri))
     r = requests.post(uri, data=post_data, allow_redirects=True, cookies=cookie_jar_, headers={"User-Agent":user_agent_, "Referer":uri})
     if r.error is None:
         cd_header = "attachment; filename="
         if "content-disposition" in r.headers and r.headers["content-disposition"].startswith(cd_header):
             filename=r.headers["content-disposition"][len(cd_header):]
-            with open(os.path.join(dstdir,filename),"wb") as fh:
+            with open(os.path.join(dstdir, filename), "wb") as fh:
                 fh.write(r.content)
                 return filename
-        raise Exception("Invalid gccode or other geocaching.com error")
-    raise Exception("Recieved HTTP Error "+str(r.status_code))
+        raise GeocachingSiteError("Invalid gccode or other geocaching.com error")
+    raise HTTPError("Recieved HTTP Error "+str(r.status_code))
 
-def gc_get_pq_names():
-  global cookie_jar_
-  uri = gc_pqlist_uri_
-  r = requests.get(uri, cookies=cookie_jar_, headers={"User-Agent":user_agent_, "Referer":uri})
-  if r.error is None:
-    rv = {}
-    tree = etree.fromstring(r.content, parser_)
-    for a_elem in tree.findall(".//a[@href]"):
-      if a_elem.get("href").startswith(gc_pqdownload_path_ % ""):
-        rv[a_elem.text.strip()] = a_elem.get("href")[len(gc_pqdownload_path_ % ""):]
-    return rv
-  raise Exception("Recieved HTTP Error "+str(r.status_code))
+def get_pq_names():
+    global cookie_jar_
+    uri = gc_pqlist_uri_
+    r = requests.get(uri, cookies=cookie_jar_, headers={"User-Agent":user_agent_, "Referer":uri})
+    if r.error is None:
+        rv = {}
+        tree = etree.fromstring(r.content, parser_)
+        for a_elem in tree.findall(".//a[@href]"):
+            if a_elem.get("href").startswith(gc_pqdownload_path_ % ""):
+                rv[a_elem.text.strip()] = a_elem.get("href")[len(gc_pqdownload_path_ % ""):]
+        return rv
+    raise HTTPError("Recieved HTTP Error "+str(r.status_code))
 
-def gc_download_pq(pquid, dstdir):
-  global cookie_jar_
-  uri = gc_pqdownload_host_ + gc_pqdownload_path_ % pquid
-  r = requests.get(uri, cookies=cookie_jar_)
-  if r.error is None:
-    cd_header = "attachment; filename="
-    if "content-disposition" in r.headers and r.headers["content-disposition"].startswith(cd_header):
-      filename=r.headers["content-disposition"][len(cd_header):]
-      with open(os.path.join(dstdir,filename),"wb") as fh:
-        fh.write(r.content)
-        return filename
-    raise Exception("Invalid PQ uid or other geocaching.com error")
-  raise Exception("Recieved HTTP Error "+str(r.status_code))
+def download_pq(pquid, dstdir):
+    global cookie_jar_
+    uri = gc_pqdownload_host_ + gc_pqdownload_path_ % pquid
+    r = requests.get(uri, cookies=cookie_jar_)
+    if r.error is None:
+        cd_header = "attachment; filename="
+        if "content-disposition" in r.headers and r.headers["content-disposition"].startswith(cd_header):
+            filename=r.headers["content-disposition"][len(cd_header):]
+            with open(os.path.join(dstdir, filename),"wb") as fh:
+                fh.write(r.content)
+                return filename
+        raise GeocachingSiteError("Invalid PQ uid or other geocaching.com error")
+    raise HTTPError("Recieved HTTP Error "+str(r.status_code))
 
-def gc_upload_fieldnote(fieldnotefileObj, ignore_previous_logs=True):
-  global cookie_jar_
-  #<input id="ctl00_ContentBody_chkSuppressDate" type="checkbox" checked="checked" name="ctl00$ContentBody$chkSuppressDate">
-  #<input id="ctl00_ContentBody_FieldNoteLoader" type="file" name="ctl00$ContentBody$FieldNoteLoader">
-  #<input id="__EVENTTARGET" type="hidden" value="" name="__EVENTTARGET">
-  #<input id="__EVENTARGUMENT" type="hidden" value="" name="__EVENTARGUMENT">
-  if not isinstance(fieldnotefileObj, file):
-    try:
-      fieldnotefileObj = open(fieldnotefileObj, "rb")
-    except:
-      return False
-
-  uri = gc_upload_uri_
-  post_data={
-        "__EVENTTARGET":"",
-        "__EVENTARGUMENT":"",
-        "ctl00$ContentBody$btnUpload":"Upload Field Note"
-    }
-  if ignore_previous_logs:
-    post_data["ctl00$ContentBody$chkSuppressDate"] = "1"
-  post_files = { "ctl00$ContentBody$FieldNoteLoader" : fieldnotefileObj }
-
-  r = requests.post(uri, data=post_data, files=post_files, allow_redirects=True, cookies=cookie_jar_, headers={"User-Agent":user_agent_, "Referer":uri})
-  if r.error is None:
-    with open("/tmp/result.html","w") as fh:
-      fh.truncate()
-      fh.write(r.content)
-    tree = etree.fromstring(r.content, parser_)
-    successdiv = tree.find(".//div[@id='ctl00_ContentBody_regSuccess']")
-    if not successdiv is None:
-      return successdiv.text.strip()
-    else:
-      raise Exception("geocaching.com did not like the provided file  %s" % fieldnotefileObj.name)
-  raise Exception("Recieved HTTP Error "+str(r.status_code))
-
+def upload_fieldnote(fieldnotefileObj, ignore_previous_logs=True):
+    global cookie_jar_
+    #<input id="ctl00_ContentBody_chkSuppressDate" type="checkbox" checked="checked" name="ctl00$ContentBody$chkSuppressDate">
+    #<input id="ctl00_ContentBody_FieldNoteLoader" type="file" name="ctl00$ContentBody$FieldNoteLoader">
+    #<input id="__EVENTTARGET" type="hidden" value="" name="__EVENTTARGET">
+    #<input id="__EVENTARGUMENT" type="hidden" value="" name="__EVENTARGUMENT">
+    if not isinstance(fieldnotefileObj, file):
+        try:
+            fieldnotefileObj = open(fieldnotefileObj, "rb")
+        except:
+            return False
+    uri = gc_uploadfieldnotes_uri_
+    post_data={
+          "__EVENTTARGET":"",
+          "__EVENTARGUMENT":"",
+          "ctl00$ContentBody$btnUpload":"Upload Field Note"
+        }
+    if ignore_previous_logs:
+        post_data["ctl00$ContentBody$chkSuppressDate"] = "1"
+    post_files = {"ctl00$ContentBody$FieldNoteLoader" : fieldnotefileObj}
+    r = requests.post(uri, data=post_data, files=post_files, allow_redirects=True, cookies=cookie_jar_, headers={"User-Agent":user_agent_, "Referer":uri})
+    if r.error is None:
+        if __debug__:
+            with open("/tmp/result.html","w") as fh:
+                fh.truncate()
+                fh.write(r.content)
+        tree = etree.fromstring(r.content, parser_)
+        successdiv = tree.find(".//div[@id='ctl00_ContentBody_regSuccess']")
+        if not successdiv is None:
+            return successdiv.text.strip()
+        else:
+            raise GeocachingSiteError("geocaching.com did not like the provided file %s" % fieldnotefileObj.name)
+    raise HTTPError("Recieved HTTP Error "+str(r.status_code))
